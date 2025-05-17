c7ffe115f6fb3fd376a05142af8d3409bef838b2
Raúl Regidor Arenales <rregidor@epo.org>
2025-05-17 21:55:43 +0200

    Fix port for Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -29,3 +30,7 @@ def scrape():
         "epo_url": url,
         "title": title
     })
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
